823cab6b0d27aac30171d9cce331ad60cc610d98
Freesia <freesia@studiofreesia.com>
2022-07-19 15:33:36 +0900

    Sentry外す

--- WindowTranslator/Program.cs
+++ WindowTranslator/Program.cs
@@ -2,7 +2,6 @@
 using Kamishibai;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using WindowTranslator;
 using WindowTranslator.Modules.Cache;
 using WindowTranslator.Modules.Capture;
@@ -15,10 +14,6 @@ CoreMessagingHelper.CreateDispatcherQueueControllerForCurrentThread();
 
 
 var builder = KamishibaiApplication<App, StartupDialog>.CreateBuilder();
-
-#if !DEBUG
-builder.Logging.AddSentry("https://5301a4e1d7cd4c45877493fd27303166@o351180.ingest.sentry.io/6471678");
-#endif
 
 builder.Host.ConfigureAppConfiguration((_, b) =>
 {
