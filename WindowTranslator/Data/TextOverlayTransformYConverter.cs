147a92e37cf7a86e877afd9812c36df8346fee93
Freesia <freesia@studiofreesia.com>
2023-03-12 19:35:22 +0900

    ちょっとだけ上にずらして表示

---
+++ WindowTranslator/Data/TextOverlayTransformYConverter.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace WindowTranslator.Data;
+
+[ValueConversion(typeof(double), typeof(double))]
+public sealed class TextOverlayTransformYConverter : IValueConverter
+{
+    public static readonly TextOverlayTransformYConverter Default = new();
+
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        var size = (double)value;
+        var rate = double.Parse(parameter.ToString() ?? "1");
+        return size * rate;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        => throw new NotImplementedException();
+}
