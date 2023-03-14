d0b00b369d1bcc92532b9a4cef7b1241a087134f
Freesia <freesia@studiofreesia.com>
2023-03-14 21:32:26 +0900

    ターゲットフレームワークを出力パスに含めないように修正

--- Plugins/copy_plugins.bat
+++ Plugins/copy_plugins.bat
@@ -4,4 +4,4 @@ set PLUGIN_DIR=%1
 set PLUGIN_NAME=%2
 set CONFIGURATION=%3
 
-xcopy /Y /I %PLUGIN_DIR% %BASE_DIR%..\WindowTranslator\bin\%CONFIGURATION%\net6.0-windows10.0.22000.0\plugins\%PLUGIN_NAME%\
+xcopy /Y /I %PLUGIN_DIR% %BASE_DIR%..\WindowTranslator\bin\%CONFIGURATION%\plugins\%PLUGIN_NAME%\
