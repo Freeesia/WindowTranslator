9eb5c4899e8e232c8931b709ddf50e3204b50e20
Freesia <freesia@studiofreesia.com>
2023-03-18 04:25:13 +0900

    フレームバッファが枯渇しないように抜けるのではなく、待つように修正

--- WindowTranslator/Modules/Capture/WindowsGraphicsCapture.cs
+++ WindowTranslator/Modules/Capture/WindowsGraphicsCapture.cs
@@ -1,4 +1,5 @@
 ﻿using Composition.WindowsRuntimeHelpers;
+using Microsoft.Extensions.Logging;
 using Microsoft.VisualStudio.Threading;
 using System.ComponentModel;
 using Windows.Foundation.Metadata;
@@ -16,15 +17,17 @@ public sealed partial class WindowsGraphicsCapture : ICaptureModule, IDisposable
     private readonly Direct3D11CaptureFramePool framePool;
     private readonly IDirect3DDevice device;
     private readonly SemaphoreSlim processing = new(1, 1);
+    private readonly ILogger<WindowsGraphicsCapture> logger;
     private GraphicsCaptureSession? session;
     private SizeInt32 lastSize = new(1000, 1000);
 
     public event AsyncEventHandler<CapturedEventArgs>? Captured;
 
-    public WindowsGraphicsCapture()
+    public WindowsGraphicsCapture(ILogger<WindowsGraphicsCapture> logger)
     {
         this.device = Direct3D11Helper.CreateDevice()!;
         this.framePool = Direct3D11CaptureFramePool.Create(device, DirectXPixelFormat.B8G8R8A8UIntNormalized, 1, lastSize);
+        this.logger = logger;
     }
 
 
@@ -39,7 +42,7 @@ public sealed partial class WindowsGraphicsCapture : ICaptureModule, IDisposable
     {
         var item = CaptureHelper.CreateItemForWindow(targetWindow)!;
         this.lastSize = item.Size;
-        this.framePool.Recreate(device, DirectXPixelFormat.B8G8R8A8UIntNormalized, 1, item.Size);
+        this.framePool.Recreate(device, DirectXPixelFormat.B8G8R8A8UIntNormalized, 1, this.lastSize);
         this.framePool.FrameArrived += FramePool_FrameArrived;
         this.session = this.framePool.CreateCaptureSession(item);
         this.session.IsCursorCaptureEnabled = false;
@@ -52,27 +55,29 @@ public sealed partial class WindowsGraphicsCapture : ICaptureModule, IDisposable
 
     private async void FramePool_FrameArrived(Direct3D11CaptureFramePool sender, object args)
     {
-        if (!await this.processing.WaitAsync(0))
-        {
-            return;
-        }
+        this.logger.LogDebug("FramePool_FrameArrived");
+        await this.processing.WaitAsync();
         using var rel = new DisposeAction(() => this.processing.Release());
+        this.logger.LogDebug("TryGetNextFrame");
         using var frame = framePool.TryGetNextFrame();
         if (frame is null)
         {
             return;
         }
-
+        this.logger.LogDebug($"フレーム取得完了:({frame.ContentSize.Width}, {frame.ContentSize.Height})");
         if (this.Captured is { } handler)
         {
             await handler.InvokeAsync(this, new(frame));
         }
-
-        if (lastSize.Width != frame.ContentSize.Width || lastSize.Height != frame.ContentSize.Height)
+        this.logger.LogDebug("イベント完了");
+        if (lastSize.Width == frame.ContentSize.Width && lastSize.Height == frame.ContentSize.Height)
         {
-            this.lastSize = frame.ContentSize;
-            framePool.Recreate(device, DirectXPixelFormat.B8G8R8A8UIntNormalized, 1, lastSize);
+            return;
         }
+        this.lastSize = frame.ContentSize;
+        this.logger.LogDebug($"フレームプール再生成:({this.lastSize.Width}, {this.lastSize.Height})");
+        framePool.Recreate(device, DirectXPixelFormat.B8G8R8A8UIntNormalized, 1, lastSize);
+        this.logger.LogDebug($"フレームプール再生成後:({this.lastSize.Width}, {this.lastSize.Height})");
     }
 
     public void StopCapture()
