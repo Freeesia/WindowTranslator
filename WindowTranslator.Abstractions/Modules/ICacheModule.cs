eac8e087500319dbd0ba3c91bf9213ef5b3b4f07
Freesia <freesia@studiofreesia.com>
2023-04-23 15:36:39 +0900

    アナライザーの有効化とドキュメントコメント設定

--- WindowTranslator.Abstractions/Modules/ICacheModule.cs
+++ WindowTranslator.Abstractions/Modules/ICacheModule.cs
@@ -1,8 +1,22 @@
 namespace WindowTranslator.Modules;
 
+/// <summary>
+/// キャッシュモジュールのインターフェースです。
+/// </summary>
 public interface ICacheModule
 {
+    /// <summary>
+    /// テキストがキャッシュに存在するかどうかを返します。
+    /// </summary>
     bool Contains(string src);
+
+    /// <summary>
+    /// 翻訳結果をキャッシュに追加します。
+    /// </summary>
     void AddRange(IEnumerable<(string src, string dst)> pairs);
+
+    /// <summary>
+    /// キャッシュから翻訳結果を取得します。
+    /// </summary>
     string Get(string src);
 }
