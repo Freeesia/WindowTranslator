368b466a9ffcbb554461a70994de54dbe602904d
Freesia <freesia@studiofreesia.com>
2023-07-10 02:47:06 +0900

    例外確認用のダミーのプラグイン入れておく

---
+++ Plugins/WindowTranslator.Plugin.DummyPlugin/DummyTranslator.cs
@@ -0,0 +1,13 @@
+﻿using WindowTranslator.Modules;
+using DisplayNameAttribute = System.ComponentModel.DisplayNameAttribute;
+
+namespace WindowTranslator.Plugin.DummyPlugin;
+
+[DisplayName("ダミー")]
+public class DummyTranslator : ITranslateModule
+{
+    public ValueTask<string[]> TranslateAsync(string[] srcTexts)
+    {
+        throw new NotImplementedException();
+    }
+}
