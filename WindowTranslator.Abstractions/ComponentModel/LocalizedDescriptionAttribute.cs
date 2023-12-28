10cb1c3862c043055362f6c1c200746b935c6c6f
Freesia <freesia@studiofreesia.com>
2023-12-28 17:15:22 +0900

    Enumの値をローカライズできるように変更

---
+++ WindowTranslator.Abstractions/ComponentModel/LocalizedDescriptionAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Concurrent;
+using System.ComponentModel;
+using System.Resources;
+
+namespace WindowTranslator.ComponentModel;
+public class LocalizedDescriptionAttribute(Type resourceType, string description) : DescriptionAttribute(description)
+{
+    private static readonly ConcurrentDictionary<Type, ResourceManager> resourceCache = new();
+    public Type ResourceType { get; set; } = resourceType;
+
+    public override string Description
+    {
+        get
+        {
+            var resourceManager = resourceCache.GetOrAdd(ResourceType, type => new ResourceManager(type));
+            return resourceManager.GetString(this.DescriptionValue) ?? base.Description;
+        }
+    }
+}
--- WindowTranslator/UserSettings.cs
+++ WindowTranslator/UserSettings.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel;
+﻿using WindowTranslator.ComponentModel;
+using WindowTranslator.Properties;
 
 namespace WindowTranslator;
 public class UserSettings
@@ -18,8 +19,8 @@ public class UserSettings
 
 public enum ViewMode
 {
-    [Description(nameof(Capture))]
+    [LocalizedDescription(typeof(Resources), nameof(Capture))]
     Capture,
-    [Description(nameof(Overlay))]
+    [LocalizedDescription(typeof(Resources), nameof(Overlay))]
     Overlay,
 }
