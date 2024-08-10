d7c41819aa36211d857caa1180a28b8c77ebd1fb
Freesia <freesia@studiofreesia.com>
2024-08-10 16:31:16 +0900

    とりあえずWinAppSDKの設定上げておく

---
+++ WindowTranslator/Modules/Ocr/WindowsAppOcr.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Windows.Imaging;
+using Microsoft.Windows.Vision;
+using Windows.Graphics.Imaging;
+
+namespace WindowTranslator.Modules.Ocr;
+internal class WindowsAppOcr : IOcrModule
+{
+    public ValueTask<IEnumerable<TextRect>> RecognizeAsync(SoftwareBitmap bitmap)
+    {
+        await TextRecognizer.MakeAvailableAsync();
+        var textRecognizer = await TextRecognizer.CreateAsync();
+
+        var options = new TextRecognizerOptions();
+
+        // create ImageBuffer from image
+        var imageBuffer = ImageBuffer.CreateBufferAttachedToBitmap(image);
+
+        var recognizedText = await textRecognizer.RecognizeTextFromImageAsync(imageBuffer, options);
+    }
+}
