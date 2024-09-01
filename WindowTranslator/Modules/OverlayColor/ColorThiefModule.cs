222aeba6a6e932e39ee98d6026c2be7c13e3a40e
Freesia <freesia@studiofreesia.com>
2024-09-02 02:03:34 +0900

    メモリコピーを減らして、ドミナントカラーを特定する領域も縮小する

--- WindowTranslator/Modules/OverlayColor/ColorThiefModule.cs
+++ WindowTranslator/Modules/OverlayColor/ColorThiefModule.cs
@@ -1,10 +1,12 @@
 ﻿using ColorThiefDotNet;
+using Microsoft.Extensions.Logging;
 using System.ComponentModel;
 using System.Drawing;
 using System.IO;
 using Windows.Graphics.Imaging;
 using Windows.Storage.Streams;
 using WindowTranslator.ComponentModel;
+using WindowTranslator.Extensions;
 using Color = System.Drawing.Color;
 using ColorConverter = ColorHelper.ColorConverter;
 
@@ -12,35 +14,36 @@ namespace WindowTranslator.Modules.OverlayColor;
 
 [DefaultModule]
 [DisplayName("近似カラー")]
-public class ColorThiefModule : IColorModule
+public class ColorThiefModule(ILogger<ColorThiefModule> logger) : IColorModule
 {
     private readonly ColorThief colorThief = new();
+    private readonly ILogger<ColorThiefModule> logger = logger;
 
     public async ValueTask<IEnumerable<TextRect>> ConvertColorAsync(SoftwareBitmap bitmap, IEnumerable<TextRect> texts)
     {
         using var ms = new InMemoryRandomAccessStream();
-        var encoder = await BitmapEncoder.CreateAsync(BitmapEncoder.BmpEncoderId, ms);
-        encoder.SetSoftwareBitmap(bitmap);
-        await encoder.FlushAsync();
-        var decoder = await BitmapDecoder.CreateAsync(ms);
         var results = new List<TextRect>(texts.Count());
+        var palette = TimeSpan.Zero;
+        var scale = Math.Clamp(Math.Max(bitmap.PixelWidth / 1280.0, bitmap.PixelHeight / 720.0), 1, double.MaxValue);
         foreach (var text in texts)
         {
-            var transform = new BitmapTransform()
-            {
-                Bounds = new((uint)text.X, (uint)text.Y, (uint)text.Width, (uint)text.Height),
-            };
-            using var crop = await decoder.GetSoftwareBitmapAsync(BitmapPixelFormat.Rgba8, BitmapAlphaMode.Ignore, transform, ExifOrientationMode.IgnoreExifOrientation, ColorManagementMode.DoNotColorManage);
-            using var cropStream = new InMemoryRandomAccessStream();
-            var cropEncoder = await BitmapEncoder.CreateAsync(BitmapEncoder.BmpEncoderId, cropStream);
-            cropEncoder.SetSoftwareBitmap(crop);
-            await cropEncoder.FlushAsync();
-            using var bmp = new Bitmap(cropStream.AsStream());
+            ms.Seek(0);
+            var encoder = await BitmapEncoder.CreateAsync(BitmapEncoder.BmpEncoderId, ms);
+            encoder.SetSoftwareBitmap(bitmap);
+            encoder.BitmapTransform.ScaledWidth = (uint)(bitmap.PixelWidth / scale);
+            encoder.BitmapTransform.ScaledHeight = (uint)(bitmap.PixelHeight / scale);
+            encoder.BitmapTransform.Bounds = new((uint)(text.X / scale), (uint)(text.Y / scale), (uint)(text.Width / scale), (uint)(text.Height / scale));
+            await encoder.FlushAsync();
 
+            // パレット取得が遅い
+            // SIMD使えば速くなりそう…
+            var now = DateTime.UtcNow;
+            using var bmp = new Bitmap(ms.AsStream());
             var colors = colorThief.GetPalette(bmp, ignoreWhite: false)
                 .OrderByDescending(c => c.Population)
                 .Select(c => c.Color)
                 .ToArray();
+            palette += DateTime.UtcNow - now;
             var back = colors[0];
 
             // 文字影が文字色より大きくなることがあるので、背景色とのBrightness距離が大きい方を文字色とする
@@ -48,6 +51,8 @@ public class ColorThiefModule : IColorModule
             var front = GetDistance(backB, colors[1]) > GetDistance(backB, colors[2]) ? colors[1] : colors[2];
             results.Add(text with { Foreground = Color.FromArgb(front.R, front.G, front.B), Background = Color.FromArgb(0xF0, back.R, back.G, back.B) });
         }
+
+        this.logger.LogDebug($"Palette:{palette}");
         return results;
     }
 
