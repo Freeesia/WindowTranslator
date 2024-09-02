5757423deb9ddb4e1c004cc9394f0ff295fd8990
Freesia <freesia@studiofreesia.com>
2024-09-02 18:56:34 +0900

    ベンチマーク追加

---
+++ ColorThief/ColorThief.Bench/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Drawing;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+using StudioFreesia.ColorThief;
+
+BenchmarkRunner.Run<ColorThiefTest>();
+
+[SimpleJob(RuntimeMoniker.HostProcess)]
+[RPlotExporter]
+public class ColorThiefTest
+{
+    private readonly ColorThiefDotNet.ColorThief colorThief = new();
+    private readonly Bitmap bmp = new("test2.jpg");
+
+    [Benchmark(Baseline = true)]
+    public List<ColorThiefDotNet.QuantizedColor> Original() => this.colorThief.GetPalette(this.bmp);
+
+    [Benchmark]
+    public List<QuantizedColor> ColorThiefEx() => ColorThief.GetPalette(bmp);
+}
