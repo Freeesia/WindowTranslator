740015ab32660d1ba2dc89f36f0587f8c33b56ba
Freesia <freesia@studiofreesia.com>
2024-09-21 15:49:29 +0900

    デバッグとリリースで異なるUserDirの定義
    デバッグビルドではユーザーディレクトリが `".wt.debug"` フォルダを指し、リリースビルドでは `".wt"` フォルダを指すように `UserDir` の定義を変更しました。

--- WindowTranslator.Abstractions/PathUtility.cs
+++ WindowTranslator.Abstractions/PathUtility.cs
@@ -8,7 +8,11 @@ public static class PathUtility
     /// <summary>
     /// ユーザー設定ディレクトリのパスを取得します。
     /// </summary>
+#if DEBUG
+    public static readonly string UserDir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".wt.debug");
+#else
     public static readonly string UserDir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".wt");
+#endif
 
     /// <summary>
     /// ユーザー設定ファイルのパスを取得します。
