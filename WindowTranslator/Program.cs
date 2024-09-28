4d95b3e259dcf387619c3a61f42d4ce03997cc42
Freesia <freesia@studiofreesia.com>
2024-09-29 00:35:59 +0900

    ビルドエラー修正

--- WindowTranslator/Program.cs
+++ WindowTranslator/Program.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using System.ComponentModel;
 using System.IO;
