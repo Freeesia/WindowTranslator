2a02117243ef4aec614c49f496ca08f92ac42523
Freesia <freesia@studiofreesia.com>
2024-11-04 00:58:49 +0900

    整理

--- WindowTranslator.Linux/App.axaml.cs
+++ WindowTranslator.Linux/App.axaml.cs
@@ -2,7 +2,7 @@ using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 
-namespace WindowTranslator.Linux;
+namespace WindowTranslator;
 
 public partial class App : Application
 {
