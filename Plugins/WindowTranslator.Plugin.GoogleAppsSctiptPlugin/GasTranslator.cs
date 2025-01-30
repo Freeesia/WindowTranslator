aa528059abec1a95fee57487c89752fb48845027
Freesia <freesia@studiofreesia.com>
2025-01-31 00:55:52 +0900

    GasTranslator クラスに Google Apps Script の翻訳機能を実装

--- Plugins/WindowTranslator.Plugin.GoogleAppsSctiptPlugin/GasTranslator.cs
+++ Plugins/WindowTranslator.Plugin.GoogleAppsSctiptPlugin/GasTranslator.cs
@@ -1,11 +1,53 @@
-﻿using WindowTranslator.Modules;
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Script.v1;
+using Google.Apis.Script.v1.Data;
+using Google.Apis.Services;
+using System.Text.Json;
+using WindowTranslator.Modules;
 
 namespace WindowTranslator.Plugin.GoogleAppsSctiptPlugin;
 
 public class GasTranslator : ITranslateModule
 {
-    public ValueTask<string[]> TranslateAsync(TextInfo[] srcTexts)
+    private static readonly string[] Scopes = ["https://www.googleapis.com/auth/script.projects"];
+    private static readonly string ScriptId = "AKfycbymXadPzkNnpd92ovwULR6gult0W9Vh94ZUgT5-2Ol87He78rWstxknlsAjVOgWRsCtPw";
+    private readonly ScriptService scriptService;
+
+    public GasTranslator()
     {
-        throw new NotImplementedException();
+        var credential = GoogleCredential.GetApplicationDefault().CreateScoped(Scopes);
+        scriptService = new ScriptService(new BaseClientService.Initializer
+        {
+            HttpClientInitializer = credential,
+            ApplicationName = "WindowTranslator",
+        });
+    }
+
+    public async ValueTask<string[]> TranslateAsync(TextInfo[] srcTexts)
+    {
+        var request = new ExecutionRequest
+        {
+            Function = "doPost",
+            Parameters =
+            [
+                new
+                {
+                    texts = srcTexts.Select(t => t.Text).ToArray(),
+                    sourceLanguage = "en",
+                    targetLanguage = "ja"
+                }
+            ]
+        };
+
+        var scriptRequest = scriptService.Scripts.Run(request, ScriptId);
+        var response = await scriptRequest.ExecuteAsync();
+
+        if (response.Error is {} error)
+        {
+            throw new Exception($"Script error: {error.Message}");
+        }
+
+        var translatedTexts = JsonSerializer.Deserialize<string[]>(response.Response["result"].ToString());
+        return translatedTexts ?? [];
     }
 }
