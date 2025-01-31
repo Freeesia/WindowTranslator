c560cf336a045478166555945d26403e2f5eb9e9
Freesia <freesia@studiofreesia.com>
2025-02-01 00:47:31 +0900

    GasTranslator クラスに言語オプションを追加し、翻訳機能のソースおよびターゲット言語を設定可能にしました

--- Plugins/WindowTranslator.Plugin.GoogleAppsSctiptPlugin/GasTranslator.cs
+++ Plugins/WindowTranslator.Plugin.GoogleAppsSctiptPlugin/GasTranslator.cs
@@ -2,6 +2,8 @@
 using Google.Apis.Script.v1;
 using Google.Apis.Script.v1.Data;
 using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using Microsoft.Extensions.Options;
 using System.Text.Json;
 using WindowTranslator.Modules;
 
@@ -12,11 +14,13 @@ public class GasTranslator : ITranslateModule
     private static readonly string[] Scopes = ["https://www.googleapis.com/auth/script.projects"];
     private static readonly string ScriptId = "AKfycbymXadPzkNnpd92ovwULR6gult0W9Vh94ZUgT5-2Ol87He78rWstxknlsAjVOgWRsCtPw";
     private readonly ScriptService scriptService;
+    private readonly LanguageOptions langOptions;
 
-    public GasTranslator()
+    public GasTranslator(IOptions<LanguageOptions> langOptions)
     {
-        var credential = GoogleCredential.GetApplicationDefault().CreateScoped(Scopes);
-        scriptService = new ScriptService(new BaseClientService.Initializer
+        this.langOptions = langOptions.Value;
+
+        this.scriptService = new ScriptService(new BaseClientService.Initializer
         {
             HttpClientInitializer = credential,
             ApplicationName = "WindowTranslator",
@@ -33,8 +37,8 @@ public class GasTranslator : ITranslateModule
                 new
                 {
                     texts = srcTexts.Select(t => t.Text).ToArray(),
-                    sourceLanguage = "en",
-                    targetLanguage = "ja"
+                    sourceLanguage = this.langOptions.Source,
+                    targetLanguage = this.langOptions.Target,
                 }
             ]
         };
@@ -42,7 +46,7 @@ public class GasTranslator : ITranslateModule
         var scriptRequest = scriptService.Scripts.Run(request, ScriptId);
         var response = await scriptRequest.ExecuteAsync();
 
-        if (response.Error is {} error)
+        if (response.Error is { } error)
         {
             throw new Exception($"Script error: {error.Message}");
         }
