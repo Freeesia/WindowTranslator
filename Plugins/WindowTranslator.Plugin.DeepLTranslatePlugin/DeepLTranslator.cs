2827e22597043fdb4357d4ddbb7b6d68feb4dfa8
Freesia <freesia@studiofreesia.com>
2025-02-11 23:38:20 +0900

    各翻訳エンジンに用語集を指定する機能追加

--- Plugins/WindowTranslator.Plugin.DeepLTranslatePlugin/DeepLTranslator.cs
+++ Plugins/WindowTranslator.Plugin.DeepLTranslatePlugin/DeepLTranslator.cs
@@ -1,10 +1,14 @@
-﻿using DeepL;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.Globalization;
+using System.Text.Json.Serialization;
+using CsvHelper;
+using CsvHelper.Configuration;
+using DeepL;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using PropertyTools.DataAnnotations;
-using System.ComponentModel.DataAnnotations;
-using System.Diagnostics;
-using System.Text.Json.Serialization;
+using ValueTaskSupplement;
 using WindowTranslator.ComponentModel;
 using WindowTranslator.Modules;
 using WindowTranslator.Stores;
@@ -14,34 +18,58 @@ namespace WindowTranslator.Plugin.DeepLTranslatePlugin;
 
 [DefaultModule]
 [DisplayName("DeepL")]
-public class DeepLTranslator(
-    IProcessInfoStore processInfo,
-    IOptionsSnapshot<DeepLOptions> deeplOptions,
-    IOptionsSnapshot<LanguageOptions> langOptions,
-    ILogger<DeepLTranslator> logger) : ITranslateModule
+public class DeepLTranslator : ITranslateModule
 {
-    private readonly Translator translator = new(deeplOptions.Value.AuthKey);
-    private readonly string sourceLang = langOptions.Value.Source[..2];
-    private readonly string targetLang = langOptions.Value.Target switch
-    {
-        "en-US" or "en-GB" or "pt-BR" or "pt-PT" => langOptions.Value.Target,
-        var t => t[..2],
-    };
-    private readonly IProcessInfoStore processInfo = processInfo;
-    private readonly ILogger<DeepLTranslator> logger = logger;
-    private string? glossaryId = null;
+    private readonly Translator translator;
+    private readonly string sourceLang;
+    private readonly string targetLang;
+    private readonly IProcessInfoStore processInfo;
+    private readonly ILogger<DeepLTranslator> logger;
+    private readonly IDictionary<string, string> glossary = new Dictionary<string, string>();
+    private AsyncLazy<string?> glossaryId = new(static () => ValueTask.FromResult<string?>(null));
     private string? context = null;
+
+    public DeepLTranslator(
+        IProcessInfoStore processInfo,
+        IOptionsSnapshot<DeepLOptions> deeplOptions,
+        IOptionsSnapshot<LanguageOptions> langOptions,
+        ILogger<DeepLTranslator> logger)
+    {
+        this.translator = new(deeplOptions.Value.AuthKey);
+        this.sourceLang = langOptions.Value.Source[..2];
+        this.targetLang = langOptions.Value.Target switch
+        {
+            "en-US" or "en-GB" or "pt-BR" or "pt-PT" => langOptions.Value.Target,
+            var t => t[..2],
+        };
+        this.processInfo = processInfo;
+        this.logger = logger;
+
+        if (File.Exists(deeplOptions.Value.GlossaryPath))
+        {
+            using var reader = new StreamReader(deeplOptions.Value.GlossaryPath);
+            using var csv = new CsvReader(reader, new CsvConfiguration(CultureInfo.InvariantCulture) { HasHeaderRecord = false });
+            foreach (var (src, dst) in csv.GetRecords<Glossary>())
+            {
+                this.glossary[src] = dst;
+            }
+            this.glossaryId = new(RegisterGlossaryAsync);
+        }
+    }
+
+    private record Glossary(string Source, string Target);
 
     public async ValueTask<string[]> TranslateAsync(TextInfo[] srcTexts)
     {
         var translated = srcTexts.Select(t => t.Text).ToArray();
         this.logger.LogDebug($"Translating {srcTexts.Length} texts.");
         var sw = Stopwatch.StartNew();
+        var glossaryId = await this.glossaryId.AsValueTask().ConfigureAwait(false);
         await Parallel.ForEachAsync(srcTexts.GroupBy(t => t.Context).ToAsyncEnumerable(), async (g, ct) =>
         {
             var context = string.Join('\n', this.context, g.Key);
             var srcs = g.Select(t => t.Text).ToArray();
-            var results = await translator.TranslateTextAsync(srcs, this.sourceLang, this.targetLang, new() { Context = context, GlossaryId = this.glossaryId }, ct)
+            var results = await translator.TranslateTextAsync(srcs, this.sourceLang, this.targetLang, new() { Context = context, GlossaryId = glossaryId }, ct)
                 .ConfigureAwait(false);
             foreach (var (s, t) in srcs.Zip(results))
             {
@@ -53,7 +81,17 @@ public class DeepLTranslator(
         return translated;
     }
 
-    public async ValueTask RegisterGlossaryAsync(IReadOnlyDictionary<string, string> glossary)
+    public ValueTask RegisterGlossaryAsync(IReadOnlyDictionary<string, string> glossary)
+    {
+        foreach (var (key, value) in glossary)
+        {
+            this.glossary.TryAdd(key, value);
+        }
+        this.glossaryId = new(RegisterGlossaryAsync);
+        return default;
+    }
+
+    private async ValueTask<string?> RegisterGlossaryAsync()
     {
         var list = await this.translator.ListGlossariesAsync().ConfigureAwait(false);
         if (list.FirstOrDefault(g => g.Name == this.processInfo.Name) is { } exist)
@@ -61,7 +99,7 @@ public class DeepLTranslator(
             await this.translator.DeleteGlossaryAsync(exist.GlossaryId).ConfigureAwait(false);
         }
         var created = await this.translator.CreateGlossaryAsync(this.processInfo.Name, this.sourceLang, this.targetLang, new(glossary, true)).ConfigureAwait(false);
-        this.glossaryId = created.GlossaryId;
+        return created.GlossaryId;
     }
 
     public void RegisterContext(string context)
@@ -77,6 +115,11 @@ public class DeepLOptions : IPluginParam
     [JsonIgnore]
     [Comment]
     public string Comment { get; } = "Translated by DeepL.(https://www.deepl.com/)";
+
+    [DisplayName("用語集パス")]
+    [FileExtensions(Extensions = ".csv")]
+    [InputFilePath(".csv", "用語集 (.csv)|*.csv")]
+    public string? GlossaryPath { get; set; }
 }
 
 
