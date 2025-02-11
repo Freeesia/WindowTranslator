908115b9957e82526adfab61012967c13595874c
Freesia <freesia@studiofreesia.com>
2025-02-11 16:52:20 +0900

    OCRモジュールをAbstractionsへ移行

---
+++ WindowTranslator.Abstractions/Modules/IOcrModule.cs
@@ -0,0 +1,14 @@
+﻿using Windows.Graphics.Imaging;
+
+namespace WindowTranslator.Modules;
+
+/// <summary>
+/// テキスト認識モジュールのインターフェース
+/// </summary>
+public interface IOcrModule
+{
+    /// <summary>
+    /// 画像からテキストを認識する
+    /// </summary>
+    ValueTask<IEnumerable<TextRect>> RecognizeAsync(SoftwareBitmap bitmap);
+}
--- WindowTranslator/Modules/Ocr/IOcrModule.cs
+++
@@ -1,7 +0,0 @@
-﻿using Windows.Graphics.Imaging;
-
-namespace WindowTranslator.Modules.Ocr;
-public interface IOcrModule
-{
-    ValueTask<IEnumerable<TextRect>> RecognizeAsync(SoftwareBitmap bitmap);
-}
