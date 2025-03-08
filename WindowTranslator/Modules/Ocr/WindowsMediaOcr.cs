1b801458ed214512db431905d6bb190095062151
Freesia <freesia@studiofreesia.com>
2025-03-08 17:56:33 +0900

    OCR 処理の例外処理とエラーメッセージの改善
    `MainViewModelBase.cs` において、OCR 処理時の例外処理が追加され、エラーメッセージが適切に表示されるようになりました。また、`WindowsMediaOcr.cs` では、画像サイズに関する制限が強化され、ユーザーに対して日本語のエラーメッセージが提供されるようになりました。

--- WindowTranslator/Modules/Ocr/WindowsMediaOcr.cs
+++ WindowTranslator/Modules/Ocr/WindowsMediaOcr.cs
@@ -41,6 +41,13 @@ public sealed partial class WindowsMediaOcr(
         // 画像の幅または高さがリサイズ後の幅または高さを超える場合はリサイズが必要
         var needScale = ((int)(this.scale * bitmap.PixelWidth) > bitmap.PixelWidth)
             || ((int)(this.scale * bitmap.PixelHeight) > bitmap.PixelHeight);
+
+        var dimension = (uint)(bitmap.PixelWidth * scale) * (uint)(bitmap.PixelHeight * scale);
+        if (dimension > OcrEngine.MaxImageDimension)
+        {
+            throw new InvalidOperationException("ウィンドウサイズが大きすぎます。対象ウィンドウのサイズを小さくするか、認識設定の拡大率を下げてください。");
+        }
+
         // 拡大率に基づくリサイズ処理
         var workingBitmap = needScale ? await ResizeSoftwareBitmapAsync(bitmap, this.scale) : bitmap;
 
