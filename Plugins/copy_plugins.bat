fca13c9d5df8acb4352e73371e1b674f68e97781
Freesia <freesia@studiofreesia.com>
2025-03-21 12:57:07 +0900

    ネイティブdllをコピーするためにサブフォルダもコピーする

--- Plugins/copy_plugins.bat
+++ Plugins/copy_plugins.bat
@@ -1,7 +1,7 @@
-@echo off
+﻿@echo off
 set BASE_DIR=%~dp0
 set PLUGIN_DIR=%1
 set PLUGIN_NAME=%2
 set CONFIGURATION=%3
 
-xcopy /Y /I %PLUGIN_DIR% %BASE_DIR%..\WindowTranslator\bin\%CONFIGURATION%\plugins\%PLUGIN_NAME%\
+xcopy /S /Y /I %PLUGIN_DIR% %BASE_DIR%..\WindowTranslator\bin\%CONFIGURATION%\plugins\%PLUGIN_NAME%\
