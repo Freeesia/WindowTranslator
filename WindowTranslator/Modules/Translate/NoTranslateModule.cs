61d37a95f6847facd11daebb0d83387a34749681
Freesia <freesia@studiofreesia.com>
2025-04-20 02:12:14 +0900

    翻訳しないオプションを本体に入れる

---
+++ WindowTranslator/Modules/Translate/NoTranslateModule.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel;
+
+namespace WindowTranslator.Modules.Translate;
+
+[DisplayName("翻訳しない")]
+public class NoTranslateModule : ITranslateModule
+{
+    public ValueTask<string[]> TranslateAsync(TextInfo[] srcTexts)
+        => ValueTask.FromResult(srcTexts.Select(s => s.Text).ToArray());
+}
