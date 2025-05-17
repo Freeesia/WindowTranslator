3893e74035a61154c1af111a09c2457f5d3be8e2
Freesia <freesia@studiofreesia.com>
2025-05-18 02:02:36 +0900

    ついでにバリデーション処理修正

--- Plugins/WindowTranslator.Plugin.LLMPlugin/LLMOptions.cs
+++ Plugins/WindowTranslator.Plugin.LLMPlugin/LLMOptions.cs
@@ -1,6 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using PropertyTools.DataAnnotations;
 using WindowTranslator.ComponentModel;
+using WindowTranslator.Modules;
 using WindowTranslator.Plugin.LLMPlugin.Properties;
 
 namespace WindowTranslator.Plugin.LLMPlugin;
@@ -38,4 +39,32 @@ public enum CorrectMode
     Text,
     [LocalizedDescription(typeof(Resources), $"{nameof(CorrectMode)}_{nameof(Image)}")]
     Image,
+}
+
+public class LLMOptionsValidator : ITargetSettingsValidator
+{
+    public ValueTask<ValidateResult> Validate(TargetSettings settings)
+    {
+        var op = settings.PluginParams.GetValueOrDefault(nameof(LLMOptions)) as LLMOptions;
+        // APIキーが設定されている場合は有効
+        if (!string.IsNullOrEmpty(op?.ApiKey))
+        {
+            return ValueTask.FromResult(ValidateResult.Valid);
+        }
+
+        // 翻訳モジュールでも補正も利用しない場合は無条件で有効
+        if (settings.SelectedPlugins[nameof(ITranslateModule)] != nameof(LLMTranslator) && (op?.CorrectMode ?? CorrectMode.None) == CorrectMode.None)
+        {
+            return ValueTask.FromResult(ValidateResult.Valid);
+        }
+
+        return ValueTask.FromResult(ValidateResult.Invalid("LLM", """
+            翻訳モジュールにLLMが選択もしくは認識補正が有効化されています。
+            
+            LLMの利用にはAPIキーが必要です。
+            「対象ごとの設定」→「LLMOptions」タブのAPIキーを設定してください。
+
+            ※ローカルLLMを利用する場合もライブラリの制約のためAPIキーが必要です。
+            """));
+    }
 }
