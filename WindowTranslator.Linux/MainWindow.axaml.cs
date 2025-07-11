1cccae2e112df9b982b4d841889a7150d1a3612a
Freesia <freesia@studiofreesia.com>
2025-07-12 03:23:01 +0900

    とりあえず起動する

---
+++ WindowTranslator.Linux/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace WindowTranslator.Linux;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
