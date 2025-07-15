2ba61c3c1bc678f54546f9b9eb01510f07dbadff
Freesia <freesia@studiofreesia.com>
2025-07-16 00:34:11 +0900

    フォントサイズに基づくYオフセット調整機能追加
    `FontSizeToOffsetYBehavior`クラスを新たに実装し、`TextBlock`のフォントサイズに応じたマージンの調整を行うビヘイビアを追加しました。これにより、UIの見た目が改善され、フォントサイズの変更に柔軟に対応できるようになります。

---
+++ WindowTranslator/Behaviors/FontSizeToOffsetYBehavior.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using Microsoft.Xaml.Behaviors;
+
+namespace WindowTranslator.Behaviors;
+
+public sealed class FontSizeToOffsetYBehavior : Behavior<TextBlock>
+{
+    protected override void OnAttached()
+    {
+        this.AssociatedObject.Loaded -= OnTextBlockLoaded;
+        this.AssociatedObject.Loaded += OnTextBlockLoaded;
+    }
+
+    protected override void OnDetaching()
+    {
+        this.AssociatedObject.Loaded -= OnTextBlockLoaded;
+    }
+
+    private void OnTextBlockLoaded(object sender, RoutedEventArgs e)
+    {
+        this.AssociatedObject.Loaded -= OnTextBlockLoaded;
+        var typeface = new Typeface(this.AssociatedObject.FontFamily, this.AssociatedObject.FontStyle, this.AssociatedObject.FontWeight, this.AssociatedObject.FontStretch);
+        if (!typeface.TryGetGlyphTypeface(out var glyph))
+        {
+            return;
+        }
+        double baseline = glyph.Baseline * this.AssociatedObject.FontSize;
+        double capHeight = glyph.CapsHeight * this.AssociatedObject.FontSize;
+        double offsetY = -(baseline - capHeight);
+        this.AssociatedObject.SetCurrentValue(FrameworkElement.MarginProperty, new Thickness(0, offsetY, 0, 0));
+    }
+}
