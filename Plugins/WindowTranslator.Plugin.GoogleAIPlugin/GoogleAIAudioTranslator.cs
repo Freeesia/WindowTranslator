be52567cecc607d6009e01df0687c30a116e46f7
Freesia <freesia@studiofreesia.com>
2025-07-21 00:23:31 +0900

    空実装追加

---
+++ Plugins/WindowTranslator.Plugin.GoogleAIPlugin/GoogleAIAudioTranslator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowTranslator.Plugin.GoogleAIPlugin;
+internal sealed class GoogleAIAudioTranslator : IDisposable
+{
+    private readonly int pid;
+
+    public GoogleAIAudioTranslator(int pid)
+    {
+        this.pid = pid;
+    }
+
+    public void Dispose()
+    {
+    }
+}
