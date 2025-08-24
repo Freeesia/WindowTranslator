f43bcfcf3a53ddad2b09480c86765a0ecd84a1fd
Freesia <freesia@studiofreesia.com>
2025-08-24 18:52:06 +0900

    「適用」ボタンでアタッチ中に変更を適用するとオーバーレイ切替機能が効かなくなる問題修正

---
+++ WindowTranslator/Extensions/KamishibaiExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Kamishibai;
+
+namespace WindowTranslator.Extensions;
+public static class KamishibaiExtensions
+{
+    public static Task<bool> CloseAsync(this IWindow window)
+    {
+        var taskSource = new TaskCompletionSource<bool>();
+        var count = 0;
+        window.Closing += (_, e) =>
+        {
+            if (e.Cancel && count > 1)
+            {
+                taskSource.SetResult(false);
+            }
+            count++;
+        };
+        window.Closed += (_, _) =>
+        {
+            taskSource.SetResult(true);
+        };
+        window.Close();
+        return taskSource.Task;
+    }
+}
