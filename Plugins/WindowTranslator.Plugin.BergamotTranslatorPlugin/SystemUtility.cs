535533ae836b99e51e0f4331acc9f719f0c416c9
Freesia <freesia@studiofreesia.com>
2025-11-30 21:17:25 +0900

    共有フォルダを利用するように変更 Fix #522

--- Plugins/WindowTranslator.Plugin.BergamotTranslatorPlugin/SystemUtility.cs
+++ Plugins/WindowTranslator.Plugin.BergamotTranslatorPlugin/SystemUtility.cs
@@ -1,10 +1,13 @@
 ﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 namespace WindowTranslator.Plugin.BergamotTranslatorPlugin;
 
 public static partial class SystemUtility
 {
+    public static string ModelsPath { get; } = Path.Combine(PathUtility.SharedDir, "bergamot");
+
     private const ushort IMAGE_FILE_MACHINE_AMD64 = 0x8664;
 
     [LibraryImport("kernel32.dll", SetLastError = true)]
@@ -18,5 +21,21 @@ public static partial class SystemUtility
             return nativeMachine == IMAGE_FILE_MACHINE_AMD64;
         }
         return false;
+    }
+
+    private static void MigrateModelsIfNeeded()
+    {
+        var oldPath = Path.Combine(PathUtility.UserDir, "bergamot");
+        if (Directory.Exists(oldPath) && !Directory.Exists(ModelsPath))
+        {
+            Directory.CreateDirectory(PathUtility.SharedDir);
+            Directory.Move(oldPath, ModelsPath);
+        }
+    }
+
+    [ModuleInitializer]
+    internal static void Initialize()
+    {
+        MigrateModelsIfNeeded();
     }
 }
